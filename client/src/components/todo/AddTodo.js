23400e59e142591238e9b9b1862ee63845daddd1
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-11 22:33:37 -0700

    Connect post requests on client side to server

--- client/src/components/todo/AddTodo.js
+++ client/src/components/todo/AddTodo.js
@@ -1,11 +1,46 @@
 import classes from './AddTodo.module.css';
+import { useState } from 'react';
 
 function AddTodo() {
+  const [form, setForm] = useState({ task: '' });
+
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    const newTask = { ...form };
+
+    await fetch('http://localhost:5000/todos/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTask),
+    }).catch((err) => {
+      console.log(err);
+      return;
+    });
+
+    setForm({ task: '' });
+  }
+
   return (
     <div className={classes.addCard}>
-      <form>
+      <form onSubmit={onSubmit}>
         <label htmlFor="task" />
-        <input type="text" id="task" placeholder="Add a task" required></input>
+        <input
+          type="text"
+          id="task"
+          placeholder="Add a task"
+          value={form.task}
+          onChange={(e) => updateForm({ task: e.target.value })}
+          required
+        ></input>
         <button>Add</button>
       </form>
     </div>
