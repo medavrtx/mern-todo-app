9b58663e07a6723f626436f0f332fe5fe0a3dbeb
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-11 21:27:59 -0700

    Connect server to database

---
+++ server/routes/todo.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const todoRoutes = express.Router();
+
+const dbo = require('../db/conn');
+
+const ObjectId = require('mongodb').ObjectId;
+
+// Fetch all todos
+todoRoutes.route('/todos').get(function (req, res) {
+  let db_connect = dbo.getDb('todo-app');
+  db_connect
+    .collection('todos')
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// Create new todo
+todoRoutes.route('/todos/add').post(function (req, response) {
+  let db_connect = dbo.getDb();
+  console.log(req.body.task);
+  let myobj = {
+    task: req.body.task,
+  };
+  db_connect.collection('todos').insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// Delete todo
+todoRoutes.route('/:id').delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection('todos').deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log('1 task deleted');
+    response.json(obj);
+  });
+});
+
+module.exports = todoRoutes;
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const cors = require('cors');
+require('dotenv').config({ path: './config.env' });
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes/todo'));
+
+const dbo = require('./db/conn');
+
+app.listen(port, () => {
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
